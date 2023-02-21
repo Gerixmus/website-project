3f78cf119f13b142a6fd1541c7a43dec994733d0
Gerixmus <gerixmuss@gmail.com>
2023-02-21 13:20:22 +0100

    fix: Fixed display for contact page

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <div style={{display: "flex", flexDirection: "column"}}>
+    <div style={{display: "flex", flexDirection: "column", height: "100%"}}>
       <AnimatePresence exitBeforeEnter>
       <ScrollToTop />
       <Navbar />
