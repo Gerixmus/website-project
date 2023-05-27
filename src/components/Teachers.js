2a235b9e4f51fa05290a6703db8d6422583cc08f
Gerixmus <gerixmuss@gmail.com>
2023-05-27 04:34:15 +0200

    refactor: Visual changes of teachers section

--- src/components/Teachers.js
+++ src/components/Teachers.js
@@ -5,10 +5,12 @@ function Teachers() {
   return (
     <div className="teachers-container">
       <div className="teachers-description">
-        <h1>Our teachers</h1>
-        <p>
-          The team of proffesional teachers will help you with your study
-          progress
+        <p className="teachers-header">Our teachers</p>
+        <h1>Team of Experts</h1>
+        <p className="motto">
+          We are highly qualified and dedicated professionals who are passionate
+          about teaching and helping students achieve their language learning
+          objectives
         </p>
       </div>
       <div className="our-teachers">
